c05965862d3ceabba5e7b685b09e84f6d7bcf77b
Hypnoscat <maksymkirianov697@gmail.com>
2025-03-31 21:04:11 +0200

    first boot

---
+++ Server/DataBaseManager.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.util.HashMap;
+
+public class DataBaseManager {
+        private final String FILENAME = "Comune-di-Roma-Capitale---Elenco-delle-strutture-ricettive.csv";
+        private ReaderDataBaseManager readerDataBaseManager;
+        private HashMap <String , SectionDataBase> listDB;
+    
+        public void initDataBase() throws IOException{
+            readerDataBaseManager = new ReaderDataBaseManager();
+            readerDataBaseManager.dataBaseReader(FILENAME);
+            listDB = readerDataBaseManager.getDataBase();
+        }
+
+        public String findDataByTipologia(String tipologia){
+            String msg = "";
+            for(java.util.Map.Entry<String, SectionDataBase> entry: listDB.entrySet()){
+                String key = entry.getKey();
+                SectionDataBase value = entry.getValue();
+                if (key.equals(tipologia)) {
+                    msg += value.getDataByTipologia()+"\n";
+                }
+            }
+            return msg;
+        }
+
+        public String findDataByCategoria(String categoriaN){
+            String msg = "";
+            for(java.util.Map.Entry<String, SectionDataBase> entry: listDB.entrySet()){
+                SectionDataBase value = entry.getValue();
+                msg += value.getDataByCategoria(categoriaN)+"\n";
+            }
+            return msg;
+        }
+
+        public String findDataByMunicipio(String municipioN , String municipioNEX){
+            String msg = "";
+            for(java.util.Map.Entry<String, SectionDataBase> entry: listDB.entrySet()){
+                SectionDataBase value = entry.getValue();
+                msg += value.getDataByMunicipio(municipioN, municipioNEX)+"\n";
+            }
+            return msg;
+        }
+}
---
+++ Server/SectionDataBase.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SectionDataBase {
+    private List<Data> lastData = new LinkedList<>();
+
+    public SectionDataBase(String[] listSectionData) {
+        addData(listSectionData);
+    }
+
+    public void addData(String[] listD) {
+        if (listD.length < 15) {
+            throw new IllegalArgumentException("Input array must have at least 15 elements.-->" + listD.length );
+        }
+
+        try {
+            lastData.add(new Data(
+                    listD[0], listD[1], listD[2], listD[3], 
+                    listD[4], listD[5], listD[6], listD[7], 
+                    listD[8],listD[9], listD[10],listD[11], 
+                    listD[12],listD[13], listD[14]));
+        } catch (NumberFormatException e) {
+            System.err.println("Error parsing integer from input data: " + e.getMessage());
+            System.err.println("Input data: " + Arrays.toString(listD));
+            throw e; // rethrow the exception after logging
+        }
+    }
+
+    public String getDataByCategoria(String categoriaN){
+        String msg ="";
+        Iterator<Data> iterator = lastData.iterator();
+        while (iterator.hasNext()){
+            Data data = iterator.next();
+            if (data.getClassificazione().equals("Categoria "+categoriaN)){
+                msg += data.toString();
+            }
+        }
+        return msg;
+    }
+
+    public String getDataByMunicipio(String municipioN ,String municipioNEX){
+        String msg ="";
+        Iterator<Data> iterator = lastData.iterator();
+        while (iterator.hasNext()){
+            Data data = iterator.next();
+            if (data.getMunicipio().equals("Municipio "+municipioN+" (ex Mun "+municipioNEX+")")){
+                //debug output
+                //System.out.println("Municipio "+municipioN+" (ex Mun "+municipioNEX+")");
+                msg += data.toString();
+            }
+        }
+        return msg;
+    }
+
+    public String getDataByTipologia(){
+        String msg ="";
+        Iterator<Data> iterator = lastData.iterator();
+        while (iterator.hasNext()){
+            Data data = iterator.next();
+            msg += data.toString();
+        }
+        return msg;
+    }
+}
